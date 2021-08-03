0f87630268b36a9d4965206f2a05b7de713254c5
Glitch (user-agent-client-hints) <none>
2021-08-03 15:47:31 +0000

    🍔😼 Checkpoint ./public/critical-ch.html:379177/132 ./server.js:379177/9

--- server.js
+++ server.js
@@ -110,9 +110,9 @@ app.get('/show-headers.json', (req, res) => {
 app.use(express.static('public'));
 
 // Cache static files in production
-if (app.get('env') === 'production') {
-  app.use(express.static('public', { maxAge: '1h' }));
-}
+// if (app.get('env') === 'production') {
+//   app.use(express.static('public', { maxAge: '1h' }));
+// }
 
 const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
